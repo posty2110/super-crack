177dab759b850334ef9cb05180a172d444e54c52
posty2110 <63021470+posty2110@users.noreply.github.com>
2022-06-28 20:38:22 +0200

    Add files via upload

---
+++ super.py
@@ -0,0 +1,31 @@
+###Welcome To Project Hack The User########
+
+import time
+import os
+
+print("Hello New User")
+
+time.sleep(1)
+
+name = input("Whats your name?")
+
+print("Thanks " + name + " For cloning Super-Crack to continue get user from owner")
+
+time.sleep(1)
+
+os.system('xdg-open https://wa.link/0ujb9i')
+
+time.sleep(2)
+
+os.system("clear")
+
+os.system("pkg install figlet")
+
+os.system("figlet 2110")
+
+os.system("python trympy")
+
+os.system("git clone https://github.com/JustAHackers/HackFB")
+
+os.system("cd H*")
+
